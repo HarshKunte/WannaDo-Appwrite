db0851044a0fc9002483136ae3c16dbf93994d6a
HarshKunte <harshkunte99@gmail.com>
2023-01-27 00:22:32 +0530

    deployment bugs fixed

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path')
 const express = require('express');
 const connectToDb = require('./config/db');
 const cors = require('cors')
@@ -10,6 +11,14 @@ app.use(express.json())
 app.use('/api/', userRoutes)
 app.use(cors())
 
+app.use(express.static(path.join(path.resolve(), '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(path.resolve(), '/frontend/build/index.html'))
+);
+
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: err.message });
+  });
 connectToDb()
 
 module.exports = app
