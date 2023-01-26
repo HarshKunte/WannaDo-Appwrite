4afafdb7b00a2e437c625c07dbfa5cb3c794df90
HarshKunte <harshkunte99@gmail.com>
2023-01-27 00:54:25 +0530

    appwrite uri updated

--- frontend/src/config/appwrite.js
+++ frontend/src/config/appwrite.js
@@ -1,7 +1,7 @@
 import { Client, Account } from "appwrite";
 
 const client = new Client()
-    .setEndpoint('https://wannado.onrender.com/')
+    .setEndpoint('https://wannado.onrender.com/v1')
     .setProject(process.env.REACT_APP_PROJECT_ID)
 
 const account = new Account(client);
